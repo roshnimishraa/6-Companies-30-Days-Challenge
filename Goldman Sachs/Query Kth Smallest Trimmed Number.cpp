7a33b79a0eedd31bcf118953cc295c09845464f1
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-15 20:19:57 +0530

    Update Query Kth Smallest Trimmed Number.cpp

--- Goldman Sachs/Query Kth Smallest Trimmed Number.cpp
+++ Goldman Sachs/Query Kth Smallest Trimmed Number.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> ans;
+        int n = nums.size();
+    for(auto &it : queries){
+    vector<pair<string, int>> v;
+for(int i=0;i<n;i++){
+    int trim = it[1];
 
+    int n1 = nums[i].size();
+// nums = ["102","473","251","814"] query = [1,2]
+// modified nums or s:  ["02","14","51","73"]
+string s = nums[i].substr(n1 - trim);
+v.push_back({s, i});
+ }
+//  after sorting: ["02","14","51","73"]
+// ans = "02" i.e the 1st smallest
+ sort(v.begin(),v.end());
+     int k = it[0];
+ ans.push_back(v[k-1].second);
+    }
+    return ans;
+    }
+};
