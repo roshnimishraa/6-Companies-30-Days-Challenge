921586f9fdffce848fcefd162bd1c0b15ca87e32
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-15 14:38:23 +0530

    Update High-Access Employees.cpp

--- Goldman Sachs/High-Access Employees.cpp
+++ Goldman Sachs/High-Access Employees.cpp
@@ -5,29 +5,29 @@ SC : O(N)
 class Solution {
 public:
     vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
-    unordered_map<string,vector<int>> mp;
-for(auto it: access_times){
-    int time = stoi(it[1]); //convert to int 
-// employee name and access time
-mp[it[0]].push_back(time);
-}
-//  sort each employee according to access times 
-for(auto &it : mp){
-    sort(it.second.begin(), it.second.end());
+        unordered_map<string,vector<int>> mp;
+ 
+    for(auto it: access_times){
+        string name = it[0];
+        int time = stoi(it[1]);
+    mp[name].push_back(time);
+    }
+
+// sort each employee according to access times 
+for(auto &it :mp){
+    sort(it.second.begin(),it.second.end());
 }
 
-//scan sorted access times to check if accessed >= 3 times
 vector<string> ans;
-for(auto &it : mp)
-{
+for(auto &it : mp){
     vector<int> &time = it.second;
+//i and i + 2 in the range of 1 hour then i + 1 also in the range of 1 hour.
 for(int i=2;i<time.size();i++){
-if(time[i] - time[i-2] < 100){
-// store employee in ans and move to another employee
-ans.push_back(it.first);
-break;
-
-}
+    // (0549 - 0532 < 1hr)
+    if(time[i] - time[i-2] < 100){
+        ans.push_back(it.first);
+        break;
+    }
 }
 }
 return ans;
