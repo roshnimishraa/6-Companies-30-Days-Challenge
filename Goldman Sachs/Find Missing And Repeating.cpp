537a5ec4c4a10b01c494f422d82ee643259ef166
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-16 22:37:30 +0530

    Create Find Missing And Repeating.cpp

---
+++ Goldman Sachs/Find Missing And Repeating.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        int repeating = -1;
+        int missing = -1;
+       int hash[n+1] = {0};
+       for(int i=0;i<n;i++){
+           hash[arr[i]]++;
+       }
+       for(int i=1;i<=n;i++){
+           if(hash[i] == 2){
+               repeating = i;
+           }
+          else if(hash[i] == 0){
+               missing = i;
+           }
+           
+    if(repeating != -1 && missing != -1){
+        break;
+    }
+       }
+    return {repeating,missing};
+    }
+};
