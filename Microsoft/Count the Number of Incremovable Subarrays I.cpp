f04b6c176464acab5175e72ba2ac2e420b1bf0c1
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-20 22:14:25 +0530

    Update Count the Number of Incremovable Subarrays I.cpp

--- Microsoft/Count the Number of Incremovable Subarrays I.cpp
+++ Microsoft/Count the Number of Incremovable Subarrays I.cpp
@@ -1,32 +1,21 @@
-
 class Solution {
 public:
-bool helper(vector<int> &nums,int start,int end)
-{
-    int n = nums.size();
-    int prev=0;
-    for(int i=0;i<n;i++){
-        if(i<=end && i>=start){
-            continue;
-        }
-        if(nums[i] <= prev){
-            //not increasing 2 1  prev=2 
-            continue;
-        }
-        prev = nums[i];
-    }
-    return true;
-}
     int incremovableSubarrayCount(vector<int>& nums) {
-        int count=0;
-    int n = nums.size();
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            if(helper(nums,i,j)== true){
-                count++;
+        int ans =0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i;j<nums.size();j++){
+                int last = -1, flag = 1;
+                for(int k=0;k<nums.size();k++){
+                    if(k>=i && k<=j) continue;
+                    if(last >= nums[k]) {
+                        flag = 0;
+                        break;
+                    }
+                    last = nums[k];
+                }
+                if(flag) ans++;
             }
         }
-    }
-    return count;
+  return ans;
     }
 };
