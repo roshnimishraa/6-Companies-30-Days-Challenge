58ad3c608519d83fb440da997577827459bfd66e
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-22 15:40:07 +0530

    Create Friends Of Appropriate Ages.cpp

---
+++ Walmart/Friends Of Appropriate Ages.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+bool request(int a,int b){
+return !(b<=0.5*a+7 || b>a || (b>100 && a<100));
+}
+    int numFriendRequests(vector<int>& ages) {
+        unordered_map<int,int> count;
+for(int &it:ages){
+    count[it]++;
+}
+int res=0;
+for(auto &a:count){
+    for(auto &b:count){
+    if(request(a.first, b.first))
+res+=a.second*(b.second-(a.first==b.first?1:0));
+
+    }
+}
+return res;
+    }
+};
