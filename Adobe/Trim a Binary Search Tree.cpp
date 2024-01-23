cd62be3bdeef62c0990566607fcdc7e5490941d5
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-24 01:24:58 +0530

    Create Trim a Binary Search Tree.cpp

---
+++ Adobe/Trim a Binary Search Tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* helper(TreeNode* root,int low,int high){
+    if(root == NULL) {
+        return NULL;
+    }
+TreeNode *leftNode = helper(root->left,low,high);
+if(leftNode!= root->left){
+    root->left = leftNode;
+}
+
+TreeNode *rightNode = helper(root->right,low,high);
+if(rightNode != root->right){
+    root->right = rightNode;
+}
+if(root->val > high || root->val < low){
+if(root->left != NULL){
+   return root->left;
+}
+else{
+    return root->right;
+}
+}
+return root;
+}
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        return helper(root,low,high);
+    }
+};
