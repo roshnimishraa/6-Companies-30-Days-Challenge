68b131b38a26b677703955070e7ce86250e5de4c
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-26 11:49:15 +0530

    Create Longest Word in Dictionary through Deleting.cpp

---
+++ Adobe/Longest Word in Dictionary through Deleting.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+bool isSubsequence(string sub,string super)
+{
+    int i=0,j=0;
+    while(i<sub.size() && j<super.size())
+    {
+        if(sub[i] == super[j])
+        i++;
+        j++;
+    }
+    return i==sub.size();
+}
+    string findLongestWord(string s, vector<string>& dictionary) {
+        string ans="";
+    for(string &word:dictionary)
+{
+if(word.size()>ans.size() || (word.size() == ans.size() && word < ans))
+{
+    if(isSubsequence(word,s))
+    {
+        ans = word;
+    }
+}    
+}
+return ans;
+    }
+};
