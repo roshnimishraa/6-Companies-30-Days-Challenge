35558461a33a4e3046c8d1af1a9fb0158f345b46
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-26 21:44:27 +0530

    Create LRU Cache.cpp

---
+++ Atlassian/LRU Cache.cpp
@@ -0,0 +1,52 @@
+Naive Approach
+
+class LRUCache {
+public:
+vector<pair<int,int>> cache;
+int n;
+    LRUCache(int capacity) {
+      n = capacity;  
+    }
+    
+    int get(int key) {
+    for(int i=0;i<cache.size();i++)
+    {
+        if(cache[i].first==key)
+        {
+            int val = cache[i].second;
+    pair<int,int> temp = cache[i];
+    cache.erase(cache.begin()+i);
+    cache.push_back(temp);
+    return val;
+        }
+    }
+    return -1;
+    }
+    
+    void put(int key, int value) {
+        for(int i=0;i<cache.size();i++)
+        {
+        if(cache[i].first == key)
+        {
+            cache.erase(cache.begin()+i);
+        cache.push_back({key,value});
+        return;
+        }
+        }
+    if(cache.size() == n)
+    {
+cache.erase(cache.begin());
+cache.push_back({key,value});
+    }
+    else{
+      cache.push_back({key,value});  
+    }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
