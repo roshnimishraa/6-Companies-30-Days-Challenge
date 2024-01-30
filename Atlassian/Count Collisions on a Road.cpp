820fed8d4b2fc02266bfaf3a8cc24589bfb351a3
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-01-30 12:16:44 +0530

    Create Count Collisions on a Road.cpp

---
+++ Atlassian/Count Collisions on a Road.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.length();
+        int left=0;
+        int right=n-1;
+while(left < n && directions[left] == 'L')
+{
+    left++;
+}
+while(right>=0 && directions[right]=='R')
+{
+    right--;
+}
+int count=0;
+for(int i=left;i<=right;i++)
+{
+    if(directions[i]!='S')
+    {
+        //R L
+        count++;
+    }
+}
+return count;
+    }
+};
